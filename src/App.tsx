12bbf78c77de95eb7639dfd789a3fab7b9ed3dd4
RAKESH32 <rakeshraidx@gmail.com>
2021-11-06 17:44:39 +0530

    Token Transfer Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 import ConnectToPhantom from './components/ConnectToPhantom';
+import { transfer } from './helpers/Airdrop'; 
+import * as web3 from "@solana/web3.js";
+
+
 function App() {
+
+  var connection = new web3.Connection(web3.clusterApiUrl("devnet"));
+
   return (
     <div className="App">
     <ConnectToPhantom />
+    <button onClick={() => transfer("C9A1ocQ4erCTzdCvrFXZknLk3DeaQgtYWGUPvEtMXd4A",(window as any).solana,"9QfXFkA8vK2QaHQE36jcbmZ111j7Yy8bkC1XJeBS6c1w",connection,2000000000)}>Transfer</button>
     </div>
   );
 }
---
+++ src/helpers/Airdrop.tsx
@@ -0,0 +1,73 @@
+//@ts-ignore
+import { Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+//@ts-ignore
+import { web3, Wallet } from "@project-serum/anchor";
+
+export async function transfer(tokenMintAddress: string, wallet: Wallet, to: string, connection: web3.Connection, amount: number) {
+  const mintPublicKey = new web3.PublicKey(tokenMintAddress);    
+  const mintToken = new Token(
+    connection,
+    mintPublicKey,
+    TOKEN_PROGRAM_ID,
+    wallet.payer // the wallet owner will pay to transfer and to create recipients associated token account if it does not yet exist.
+  );
+        
+  const fromTokenAccount = await mintToken.getOrCreateAssociatedAccountInfo(
+    wallet.publicKey
+  );
+
+  const destPublicKey = new web3.PublicKey(to);
+
+  // Get the derived address of the destination wallet which will hold the custom token
+  const associatedDestinationTokenAddr = await Token.getAssociatedTokenAddress(
+    mintToken.associatedProgramId,
+    mintToken.programId,
+    mintPublicKey,
+    destPublicKey 
+  );
+
+  const receiverAccount = await connection.getAccountInfo(associatedDestinationTokenAddr);
+        
+  const instructions: web3.TransactionInstruction[] = [];  
+
+  if (receiverAccount === null) {
+
+    instructions.push(
+      Token.createAssociatedTokenAccountInstruction(
+        mintToken.associatedProgramId,
+        mintToken.programId,
+        mintPublicKey,
+        associatedDestinationTokenAddr,
+        destPublicKey,
+        wallet.publicKey
+      )
+    )
+
+  }
+  
+  instructions.push(
+    Token.createTransferInstruction(
+      TOKEN_PROGRAM_ID,
+      fromTokenAccount.address,
+      associatedDestinationTokenAddr,
+      wallet.publicKey,
+      [],
+      amount
+    )
+  );
+
+  const transaction = new web3.Transaction().add(...instructions);
+  transaction.feePayer = wallet.publicKey;
+  transaction.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
+  
+  let signedTrans = await wallet.signTransaction(transaction);
+  const transactionSignature = await connection.sendRawTransaction(
+    signedTrans.serialize(),
+    { skipPreflight: true }
+    
+  );
+
+  console.log(transactionSignature);
+
+  await connection.confirmTransaction(transactionSignature);
+}
